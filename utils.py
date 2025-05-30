c7deaacf922ce8f6e73220bdfe83c428b5956c61
abduganiyevv <firdavsabduganiyev07@gmail.com>
2025-05-30 20:58:09 +0500

    bajarildi

---
+++ utils.py
@@ -0,0 +1,38 @@
+def add_task(tasks):
+    task = input("Enter the task: ")
+    tasks.append({"task": task, "done": False})
+    print("✅ Task added!")
+
+
+def view_tasks(tasks):
+    if not tasks:
+        print("📭 No tasks yet.")
+        return
+    for i, t in enumerate(tasks):
+        status = "✅" if t["done"] else "❌"
+        print(f"{i + 1}. {status} {t['task']}")
+
+def mark_done(tasks):
+    view_tasks(tasks)
+    try:
+        index = int(input("Enter task number to mark as done: ")) - 1
+        if 0 <= index < len(tasks):
+            tasks[index]["done"] = True
+            print("🎉 Task marked as done!")
+        else:
+            print("⚠️ Invalid task number.")
+    except ValueError:
+        print("❌ Please enter a number.")
+
+
+def delete_task(tasks):
+    view_tasks(tasks)
+    try:
+        index = int(input("Enter task number to delete: ")) - 1
+        if 0 <= index < len(tasks):
+            removed = tasks.pop(index)
+            print(f"🗑️ Deleted task: {removed['task']}")
+        else:
+            print("⚠️ Invalid task number.")
+    except ValueError:
+        print("❌ Please enter a number.")
